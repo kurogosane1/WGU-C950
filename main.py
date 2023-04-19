bf2e7bad3b8bc7749cd3fd2fccb05a1539f16361
Saad <magnite@hotmail.com>
2023-04-19 12:05:54 -0500

    small change

--- main.py
+++ main.py
@@ -1,10 +1,7 @@
 # Syed Khurshid, SID:010081191
 
-
 from menu import userMenu
 
-
-
 class Main:
-    # A Seperate User Menue is trigerred for easy functionality
+    # A Seperate User Menue is trigerred for easy functionality 
     userMenu()
